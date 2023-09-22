465684f219799bf04e33f4ddb5a38cf4f0cccaa3
Thomas <thomas.mortelette@outlook.fr>
2023-09-22 16:26:02 +0200

    feat: Ajout du système de modal

---
+++ src/components/Modal/ModalFooter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const ModalFooter = () => {
+
+    return(
+        <ModalFooterContainer>
+        
+        </ModalFooterContainer>
+    )
+}
+
+export default ModalFooter;
+
+const ModalFooterContainer = styled.div`
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    border-top: 1.5px solid #d3d3d3;
+`;
